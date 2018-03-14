40f6e782fea80ac791b80a72d09c32ba76103199
Jesus Guzman <jesus.guzman@twylahelps.com>
2018-03-14 17:16:17 +0100

    added mocking of os environ to two unit tests

--- twyla/service/test/unit/test_configuration.py
+++ twyla/service/test/unit/test_configuration.py
@@ -14,11 +14,12 @@ import twyla.service.configuration as config
 
 
 class ConfigurationTest(unittest.TestCase):
-    def test_configuration(self):
-        os.environ['TWYLA_TEST_KEY'] = 'some-value'
-        os.environ['TWYLA_TEST_KEY2'] = 'some-other-value'
-        os.environ['TWYLA_TEST2_KEY2'] = 'not-considered'
 
+    @mock.patch('twyla.service.configuration.os.environ', new={
+        'TWYLA_TEST_KEY': 'some-value',
+        'TWYLA_TEST_KEY2':  'some-other-value',
+        'TWYLA_TEST2_KEY2': 'not-considered'})
+    def test_configuration(self):
         prefixes = ['TWYLA_TEST', 'TWYLA_TEST_']
         expected = {'key': 'some-value',
                     'key2': 'some-other-value'}
@@ -33,12 +34,13 @@ class ConfigurationTest(unittest.TestCase):
         del os.environ['TWYLA_TEST2_KEY2']
 
 
+    @mock.patch('twyla.service.configuration.os.environ', new={
+        'TWYLA_TEST_TEST': 'some-value',
+        'TWYLA_TEST2_TEST': 'not-considered'})
     def test_configuration_prefix_eq_key(self):
         """
         This was added due to a bug where lstrip was used
         """
-        os.environ['TWYLA_TEST_TEST'] = 'some-value'
-        os.environ['TWYLA_TEST2_TEST'] = 'not-considered'
 
         prefixes = ['TWYLA_TEST', 'TWYLA_TEST_']
         expected = {'test': 'some-value'}
@@ -58,9 +60,9 @@ class ConfigurationTest(unittest.TestCase):
         assert context.value.args[0] == 'No conf set in environment; set it with key ENV_KEY'
 
 
-    @mock.patch('twyla.service.configuration.os.environ',
-                new={'ENV_KEY': '/file/name.yml',
-                     'TWYLA_SOME_CONFIG': 'some_config_value'})
+    @mock.patch('twyla.service.configuration.os.environ', new={
+        'ENV_KEY': '/file/name.yml',
+        'TWYLA_SOME_CONFIG': 'some_config_value'})
     @mock.patch('twyla.service.configuration.open', mock.mock_open(read_data=CONF))
     def test_load_config(self):
         conf = config.load_config('ENV_KEY')
@@ -68,8 +70,8 @@ class ConfigurationTest(unittest.TestCase):
                         'SOME_CONFIG': 'some_config_value'}
 
 
-    @mock.patch('twyla.service.configuration.os.environ',
-                new={'ENV_KEY': '/file/name.yml'})
+    @mock.patch('twyla.service.configuration.os.environ', new={
+        'ENV_KEY': '/file/name.yml'})
     @mock.patch('twyla.service.configuration.open', mock.mock_open(read_data=CONF))
     def test_load_config_keys_from_env(self):
         conf = config.load_config('ENV_KEY')
