6f5de6b631d0e2d4cfe611c3ce0ff8831475c04a
Ulas Turkmen <ulas.turkmen@twylahelps.com>
2018-05-09 17:05:51 +0200

    Removed unused function, added arg to load keys from env

--- twyla/service/configuration.py
+++ twyla/service/configuration.py
@@ -12,21 +12,14 @@ def from_env(prefix):
 
     for key, value in os.environ.items():
         if key.startswith(prefix):
-            from_env[key[len(prefix):].lower()] = value
+            shorter_key = key[len(prefix):]
+            from_env[shorter_key] = value
+            from_env[shorter_key.lower()] = value
     return from_env
 
 
-def config_from_env(prefix):
-    """If any environment variable starts with prefix, strip it and set
-    the value as configuration"""
-    from_env = {}
-    for key, value in os.environ.items():
-        if key.startswith(prefix):
-            from_env[key.lstrip(prefix)] = value
-    return from_env
 
-
-def load_config(env_key):
+def load_config(env_key, env_config_prefix=None):
     try:
         conf_file = os.environ[env_key]
     except KeyError:
@@ -34,6 +27,6 @@ def load_config(env_key):
         raise RuntimeError(err) from None
     with open(conf_file, 'r') as conf_file:
         configuration = yaml.load(conf_file.read())
-    configuration.update(config_from_env('TWYLA_'))
+    if env_config_prefix is not None:
+        configuration.update(from_env(env_config_prefix))
     return configuration
-
--- twyla/service/test/unit/test_configuration.py
+++ twyla/service/test/unit/test_configuration.py
@@ -17,32 +17,32 @@ class ConfigurationTest(unittest.TestCase):
 
     @mock.patch('twyla.service.configuration.os.environ', new={
         'TWYLA_TEST_KEY': 'some-value',
-        'TWYLA_TEST_KEY2':  'some-other-value',
-        'TWYLA_TEST2_KEY2': 'not-considered'})
-    def test_configuration(self):
-        prefixes = ['TWYLA_TEST', 'TWYLA_TEST_']
-        expected = {'key': 'some-value',
-                    'key2': 'some-other-value'}
-
-        for prefix in prefixes:
-            conf = config.from_env(prefix=prefix)
-            assert conf == expected
+        'TWYLA_TEST_KEY2':  'some-other-value'})
+    def test_configuration_from_env(self):
+        """
+        from_env should read all configurations, and save them with lower and uppercase
+        """
+        expected = {'test_key': 'some-value',
+                    'TEST_KEY': 'some-value',
+                    'test_key2': 'some-other-value',
+                    'TEST_KEY2': 'some-other-value'}
+        conf = config.from_env(prefix='TWYLA_')
+        assert conf == expected
 
 
     @mock.patch('twyla.service.configuration.os.environ', new={
         'TWYLA_TEST_TEST': 'some-value',
         'TWYLA_TEST2_TEST': 'not-considered'})
-    def test_configuration_prefix_eq_key(self):
+    def test_configuration_prefix_without_underscore(self):
         """
-        This was added due to a bug where lstrip was used
+        Underscore should be suffixed to the prefix if it's not already there
         """
+        expected = {'test': 'some-value', 'TEST': 'some-value'}
+        conf = config.from_env(prefix='TWYLA_TEST')
+        assert conf == expected
 
-        prefixes = ['TWYLA_TEST', 'TWYLA_TEST_']
-        expected = {'test': 'some-value'}
-
-        for prefix in prefixes:
-            conf = config.from_env(prefix=prefix)
-            assert conf == expected
+        conf = config.from_env(prefix='TWYLA_TEST_')
+        assert conf == expected
 
 
     @mock.patch('twyla.service.configuration.os.environ', new={})
@@ -56,15 +56,25 @@ class ConfigurationTest(unittest.TestCase):
         'ENV_KEY': '/file/name.yml',
         'TWYLA_SOME_CONFIG': 'some_config_value'})
     @mock.patch('twyla.service.configuration.open', mock.mock_open(read_data=CONF))
-    def test_load_config(self):
-        conf = config.load_config('ENV_KEY')
+    def test_load_config_prefix_set(self):
+        """
+        If the env_config_prefix argument is given, the configuration values should
+        also be read from the environment
+        """
+        conf = config.load_config('ENV_KEY', env_config_prefix='TWYLA')
         assert conf == {'a_key': 'a_value',
+                        'some_config': 'some_config_value',
                         'SOME_CONFIG': 'some_config_value'}
 
 
     @mock.patch('twyla.service.configuration.os.environ', new={
-        'ENV_KEY': '/file/name.yml'})
+        'ENV_KEY': '/file/name.yml',
+        'TWYLA_SOME_CONFIG': 'some_config_value'})
     @mock.patch('twyla.service.configuration.open', mock.mock_open(read_data=CONF))
-    def test_load_config_keys_from_env(self):
+    def test_load_config_no_prefix(self):
+        """
+        If the env_config_prefix argument is not given, the configuration values
+        should NOT be read from the environment
+        """
         conf = config.load_config('ENV_KEY')
         assert conf == {'a_key': 'a_value'}
